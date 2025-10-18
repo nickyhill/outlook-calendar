1ecdbd06632187cd7390b45576f116f6269c7c8a
nickyhill <nhillengas@vt.edu>
2025-10-18 17:41:13 -0400

    More testing

--- killchrome.py
+++ killchrome.py
@@ -7,7 +7,7 @@ def kill_leftover_chrome():
         # subprocess.run(["pkill", "-f", "chromedriver"], capture_output=True)
         # # Optionally kill any Chrome instances started by Selenium
         # subprocess.run(["pkill", "-f", "chrome"], capture_output=True)
-        subprocess.run(["kill", "-9", "$(sudo lsof -t -i:9222)"], capture_output=True)
+        subprocess.run(["kill", "-9", "$(sudo lsof -t -i:9222)"], capture_output=True, shell=True)
 
     except Exception as e:
         print(f"Failed to kill leftover Chrome processes: {e}", file=sys.stderr)
