09640271b5088491b63b80def2f7b75656dc07fe
nickyhill <nhillengas@vt.edu>
2025-10-18 17:45:01 -0400

    Fixed .run issues

--- killchrome.py
+++ killchrome.py
@@ -4,10 +4,14 @@ import sys
 def kill_leftover_chrome():
     try:
         # # Kill any running chromedriver processes
-        # subprocess.run(["pkill", "-f", "chromedriver"], capture_output=True)
+        success = subprocess.run(["pkill", "-f", "chromedriver"], capture_output=True, shell=True)
         # # Optionally kill any Chrome instances started by Selenium
-        # subprocess.run(["pkill", "-f", "chrome"], capture_output=True)
-        subprocess.run(["kill", "-9", "$(sudo lsof -t -i:9222)"], capture_output=True, shell=True)
+        success_second = subprocess.run(["pkill", "-f", "chrome"], capture_output=True, shell=True)
+        final = subprocess.run(["kill", "-9", "$(sudo lsof -t -i:9222)"], capture_output=True, shell=True)
+
+        print(f"Killed leftover Chrome driver processes: {success.stdout}")
+        print(f"Killed leftover Chrome processes: {success_second.stdout}")
+        print(f"Killed leftover :9222 processes: {final.stdout}")
 
     except Exception as e:
         print(f"Failed to kill leftover Chrome processes: {e}", file=sys.stderr)
