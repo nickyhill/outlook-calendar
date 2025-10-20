53fcbbc4931166016a189b92b68c32ba3002d07f
nickyhill <nhillengas@vt.edu>
2025-10-20 15:15:14 -0400

    no sudo

--- killchrome.py
+++ killchrome.py
@@ -7,7 +7,7 @@ def kill_leftover_chrome():
         success = subprocess.run(["pkill", "-f", "chromedriver"], capture_output=True, shell=True)
         # # Optionally kill any Chrome instances started by Selenium
         success_second = subprocess.run(["pkill", "-f", "chrome"], capture_output=True, shell=True)
-        final = subprocess.run(["kill", "-9", "$(sudo lsof -t -i:9222)"], capture_output=True, shell=True)
+        final = subprocess.run(["kill", "-9", "$(lsof -t -i:9222)"], capture_output=True, shell=True)
 
         print(f"Killed leftover Chrome driver processes: {success.stdout}")
         print(f"Killed leftover Chrome processes: {success_second.stdout}")
